cc9a4e41bb3a6db29d43b659d887468cc2ffd633
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-08-24 16:23:37 -0300

    Estado inicial do projeto

---
+++ src/main/java/med/voll/api_rest_spring_boot_3/ApiRestSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package med.voll.api_rest_spring_boot_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringBoot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/med/voll/api_rest_spring_boot_3/ApiRestSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package med.voll.api_rest_spring_boot_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
