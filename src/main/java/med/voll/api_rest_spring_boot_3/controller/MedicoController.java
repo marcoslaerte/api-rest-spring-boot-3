fa7e2b1bf81cf88dc28750e0c86b6367d49b84b2
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-08-26 16:36:53 -0300

    Adicionando tamanho e ordenação default no controller caso o cliente não informe na url

--- src/main/java/med/voll/api_rest_spring_boot_3/controller/MedicoController.java
+++ src/main/java/med/voll/api_rest_spring_boot_3/controller/MedicoController.java
@@ -8,10 +8,9 @@ import med.voll.api_rest_spring_boot_3.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -27,7 +26,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listart(Pageable paginacao) {
+    public Page<DadosListagemMedico> listart(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
