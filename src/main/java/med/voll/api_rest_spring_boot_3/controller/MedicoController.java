e38d404f9b0df4312771413178be7b030e25a92f
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-08-26 15:35:39 -0300

    Adicionando a listagem de médicos

--- src/main/java/med/voll/api_rest_spring_boot_3/controller/MedicoController.java
+++ src/main/java/med/voll/api_rest_spring_boot_3/controller/MedicoController.java
@@ -2,14 +2,14 @@ package med.voll.api_rest_spring_boot_3.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api_rest_spring_boot_3.medico.DadosCadastroMedico;
+import med.voll.api_rest_spring_boot_3.medico.DadosListagemMedico;
 import med.voll.api_rest_spring_boot_3.medico.Medico;
 import med.voll.api_rest_spring_boot_3.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,5 +22,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listart() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
