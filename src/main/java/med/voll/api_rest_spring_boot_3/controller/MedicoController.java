4e83c2cc474eb9e88b1f6fce9961390e493e7c47
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-08-26 15:45:47 -0300

    Adicionando paginação na listagem de médicos

--- src/main/java/med/voll/api_rest_spring_boot_3/controller/MedicoController.java
+++ src/main/java/med/voll/api_rest_spring_boot_3/controller/MedicoController.java
@@ -6,6 +6,8 @@ import med.voll.api_rest_spring_boot_3.medico.DadosListagemMedico;
 import med.voll.api_rest_spring_boot_3.medico.Medico;
 import med.voll.api_rest_spring_boot_3.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +27,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listart() {
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listart(Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
