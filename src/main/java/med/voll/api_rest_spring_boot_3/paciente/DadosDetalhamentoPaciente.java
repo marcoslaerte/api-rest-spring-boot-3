cebd7d24bf9ecc2cbe9ec50e9d9f77aa7a0fa67c
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-09-04 17:06:17 -0300

    Retorno HTTP de código 204 e 201 para pacientes no controller

---
+++ src/main/java/med/voll/api_rest_spring_boot_3/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,10 @@
+package med.voll.api_rest_spring_boot_3.paciente;
+
+import med.voll.api_rest_spring_boot_3.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(Long id, String nome, String email, String cpf) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+    }
+}
--- src/main/java/med/voll/api_rest_spring_boot_3/paciente/DadosListagemPaciente.java
+++ src/main/java/med/voll/api_rest_spring_boot_3/paciente/DadosListagemPaciente.java
@@ -1,7 +1,9 @@
 package med.voll.api_rest_spring_boot_3.paciente;
 
-public record DadosListagemPaciente(Long id, String nome, String email, String cpf) {
+import med.voll.api_rest_spring_boot_3.endereco.Endereco;
+
+public record DadosListagemPaciente(Long id, String nome, String email, String cpf, Endereco endereco) {
     public DadosListagemPaciente(Paciente paciente) {
-        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getEndereco());
     }
 }
