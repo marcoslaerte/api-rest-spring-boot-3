e1def37f62da2fb107f963cf775a627c490cc227
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-09-04 20:20:41 -0300

    Criando a tabela usuarios

---
+++ src/main/java/med/voll/api_rest_spring_boot_3/usuario/Usuario.java
@@ -0,0 +1,23 @@
+package med.voll.api_rest_spring_boot_3.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+}
