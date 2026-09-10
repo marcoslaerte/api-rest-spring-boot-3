0af1fc7c9199628971541ee73a8c40b5a89d1387
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-09-10 15:23:41 -0300

    Definindo Issuer em uma variável estática pois deve ser idêntico tanto em gerarToken quanto em getSubject

--- src/main/java/med/voll/api_rest_spring_boot_3/infra/security/TokenService.java
+++ src/main/java/med/voll/api_rest_spring_boot_3/infra/security/TokenService.java
@@ -18,11 +18,13 @@ public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
 
+    private static final String ISSUER = "API Voll.med";
+
     public String gerarToken(Usuario usuario) {
         try {
             Algorithm algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
-                    .withIssuer("API Voll.med")
+                    .withIssuer(ISSUER)
                     .withSubject(usuario.getLogin())
                     .withClaim("id", usuario.getId())
                     .withExpiresAt(dataExpiracao())
@@ -36,7 +38,7 @@ public class TokenService {
         try {
             Algorithm algoritmo = Algorithm.HMAC256(secret);
             return JWT.require(algoritmo)
-                    .withIssuer("API Voll.med")
+                    .withIssuer(ISSUER)
                     .build()
                     .verify(tokenJWT)
                     .getSubject();
