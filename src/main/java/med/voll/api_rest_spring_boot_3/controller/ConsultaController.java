d1d0dca638c340d8b4159a933cf117ce941702d6
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-09-15 19:35:53 -0300

    Personalização da documentação gerada pelo SpringDoc para a inclusão do token de autenticação

--- src/main/java/med/voll/api_rest_spring_boot_3/controller/ConsultaController.java
+++ src/main/java/med/voll/api_rest_spring_boot_3/controller/ConsultaController.java
@@ -1,5 +1,6 @@
 package med.voll.api_rest_spring_boot_3.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api_rest_spring_boot_3.domain.consulta.AgendaDeConsultas;
 import med.voll.api_rest_spring_boot_3.domain.consulta.DadosAgendamentoConsulta;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
--- src/main/java/med/voll/api_rest_spring_boot_3/controller/MedicoController.java
+++ src/main/java/med/voll/api_rest_spring_boot_3/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api_rest_spring_boot_3.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api_rest_spring_boot_3.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("medicos")
+@SecurityRequirement(name = "bearer-key")
 public class MedicoController {
 
     @Autowired
